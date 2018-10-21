25b46cedcc08b06f5edb36fccc1f4c4494bdf174
Ben Cuan <ben@bananiumlabs.com>
2018-10-21 03:32:40 -0700

    Background gradient and navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 //Init parallax
 var banner = document.getElementById('banner');
 var parallaxInstance = new Parallax(banner);
+
+// Setup waypoints
+var home = new Waypoint({
+  element: document.getElementById('banner'),
+  handler: function(direction) {
+    console.log('hi');
+  }
+})
+
+var about = new Waypoint({
+  element: document.getElementById('about'),
+  handler: function(direction) {
+    console.log('about');
+  }
+})
