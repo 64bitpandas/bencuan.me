5b28ff0faf0a97494d21baf21814d93914385ad9
Ben Cuan <ben@bananiumlabs.com>
2018-10-21 11:07:03 -0700

    Upgrade footer and navbar

--- js/main.js
+++ js/main.js
@@ -4,18 +4,35 @@ var parallaxInstance = new Parallax(banner);
 
 // Setup waypoints
 var home = new Waypoint({
-  element: document.getElementById('banner'),
+  element: document.getElementById('banner-name'),
   handler: function(direction) {
-    console.log('hi');
+    clearSelections();
+    $('#home-link').addClass('selected');
   }
 })
 
 var about = new Waypoint({
   element: document.getElementById('about'),
   handler: function(direction) {
-    console.log('about');
+    clearSelections();
+    $('#about-link').addClass('selected');
   }
 })
+
+var footer = new Waypoint({
+  element: document.getElementById('footer'),
+  handler: function(direction) {
+    clearSelections();
+    $('#contact-link').addClass('selected');
+  }
+})
+
+/**
+ * Clears the '.selected' class on all links on the banner.
+ */
+function clearSelections() {
+  $('.selected').removeClass('selected');
+}
 
 
 // Load footer
