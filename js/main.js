034bcfccb9111259935663987a53130818a06c7e
Ben Cuan <github@bencuan.me>
2019-07-12 10:59:38 -0700

    Buttons

--- js/main.js
+++ js/main.js
@@ -2,9 +2,29 @@
 if (window.location.protocol === 'https:') //remote
   $('base').attr('href', '/bencuan.me/');
 
-// Button click action to show content
-$(document).ready(() => {
-  $('launch-content-button').click(() => {
-    console.log('hi');
+  $(document).ready(() => {
+  // Button click action to show content
+  $('#launch-content-button').click(() => {
+    $('#content').css('animation', 'launch-content 1.5s ease-in 0s 1 forwards');
   })
+
+
+    $('.expand-button').hover(function() {
+      $('.expand-button-content', this).delay(300).fadeIn(200).removeClass('hidden');
+    }, function () {
+        $('.expand-button-content', this).fadeOut(10);
+    });
+
+
+
+  // Buttons to link to other sites
+  $('#github-button').click (() => {openUrl('//github.com/64bitpandas');});
+  $('#linkedin-button').click (() => {openUrl('//linkedin.com/in/bencuan');});
+  $('#email-button').click (() => {openUrl('mailto:hello@bencuan.me');});
 });
+
+
+
+function openUrl(url) {
+  window.open(url, '_blank').focus();
+}
