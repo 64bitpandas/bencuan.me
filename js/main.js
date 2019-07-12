5aed5ea958d89023c4e87cd3b267b05a10907f5a
Ben Cuan <github@bencuan.me>
2019-07-12 12:45:13 -0700

    Dynamic button label widths

--- js/main.js
+++ js/main.js
@@ -5,19 +5,24 @@ if (window.location.protocol === 'https:') //remote
   $(document).ready(() => {
   // Button click action to show content
   $('#launch-content-button').click(() => {
-    $('#content').css('animation', 'launch-content 1.5s ease-in 0s 1 forwards');
+    $('#content').css('animation', 'launch-content 1s ease-in-out 0s 1 forwards');
   })
 
 
     $('.expand-button').hover(function() {
       setTimeout(() => {
-        console.log($('.expand-button:hover'));
-        if($('.expand-button:hover').length > 0)
-          $('.expand-button-content', this).fadeIn(200).removeClass('hidden');
-      }, 300);
+        // Check to make sure button is still hovered over before fully expanding
+        if($('#' + $(this).attr('id') + ':hover').length > 0) {
+          $('.expand-button-content', this).css('width', 'auto');
+          $('.expand-button-content', this).fadeIn(500).removeClass('hidden').css('display', 'inline-block');
+        }
+      }, 100);
     }, function () {
-      if($('.expand-button-content', this).css('display') !== 'none')
+      // Don't fade out if it isn't visible in the first place!
+      if($('.expand-button-content', this).css('display') !== 'none') {
+        $('.expand-button-content', this).css('width', '0%');
         $('.expand-button-content', this).fadeOut(10);
+      }
     });
 
 
