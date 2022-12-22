8a0f1f4c97116aec223ec37d507792e740b579d2
Ben Cuan <github@bencuan.me>
2022-12-22 06:53:56 +0000

    add favicon, fix archives and fontawesome

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon-round.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
@@ -49,6 +49,5 @@ module.exports = {
         siteUrl: 'https://bencuan.me'
       }
     },
-    `gatsby-plugin-fontawesome-css`
   ],
 }
