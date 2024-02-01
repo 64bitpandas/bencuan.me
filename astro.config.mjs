9330ecb3d0954a066519f14f976cdff623c86143
Ben Cuan <github@bencuan.me>
2024-02-01 09:31:29 -0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import netlify from "@astrojs/netlify";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://bencuan.me',
+  integrations: [react(), sitemap()],
   output: "server",
   adapter: netlify()
 });
