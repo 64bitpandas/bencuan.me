f2349357180671d8e037a53edecbaf7afdd1636f
Ben Cuan <github@bencuan.me>
2024-02-01 07:34:28 +0000

    visitor interaction

---
+++ src/components/button.tsx
@@ -0,0 +1,19 @@
+import type { MouseEventHandler, ReactNode } from "react"
+import "../sass/button.scss";
+
+type props = {
+    href?: string,
+    onClick?: MouseEventHandler,
+    className?: string,
+    children?: ReactNode,
+}
+const Button = ({href, onClick, className, children}: props) => {
+
+    if (href) {
+        <a href={href} className={`button ${className}`} target="_blank" rel="noopener">{children}</a>
+    } else {
+        return (<span className={`button ${className}`} onClick={onClick}>{children}</span>)
+    }
+}
+
+export default Button
