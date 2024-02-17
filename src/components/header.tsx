4af9f796d44f74108308d2859be04b8172326661
Ben Cuan <github@bencuan.me>
2024-02-17 20:22:48 +0000

    fun buttons and good prettier

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,105 +1,115 @@
 import * as React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCircle } from "@fortawesome/free-solid-svg-icons"
-import "../sass/layout.scss";
-import "../sass/header.scss";
-import { XLink } from "./links";
+import "../sass/layout.scss"
+import "../sass/header.scss"
+import { XLink } from "./links"
 
 const links = [
   {
     name: "home",
-    url: "/"
+    url: "/",
   },
   {
     name: "blog",
-    url: "/blog"
+    url: "/blog",
   },
   {
     name: "about",
-    url: "/about"
+    url: "/about",
   },
 ]
 
 const moreLinks = [
   {
     name: "projects",
-    url: "/projects"
+    url: "/projects",
   },
   {
     name: "library",
-    url: "/library"
+    url: "/library",
   },
   {
     name: "recipes",
-    url: "/recipes"
+    url: "/recipes",
   },
   {
     name: "services",
-    url: "https://status.tsh.sh"
+    url: "https://status.tsh.sh",
   },
   {
     name: "games",
-    url: "https://hexcaliber.dev"
+    url: "https://hexcaliber.dev",
   },
 ]
 
 type props = {
-  siteTitle: string,
+  siteTitle: string
   currPage: string
 }
 
 const Header = ({ siteTitle, currPage }: props) => {
-  const [mobileNavVisible, setMobileNavVisible] = React.useState(false);
-  const [moreMenuVisible, setMoreMenuVisible] = React.useState(false);
+  const [mobileNavVisible, setMobileNavVisible] = React.useState(false)
+  const [moreMenuVisible, setMoreMenuVisible] = React.useState(false)
   const mobileClick = () => {
-    setMobileNavVisible(!mobileNavVisible);
+    setMobileNavVisible(!mobileNavVisible)
   }
 
   return (
     <header>
-      <div className={`nav-container ${(mobileNavVisible) ? "mobile-visible" : "mobile-hidden"}`}>
+      <div className={`nav-container ${mobileNavVisible ? "mobile-visible" : "mobile-hidden"}`}>
         <div className="nav-links">
-          {
-            links.map((link, i, row) => (
+          {links.map((link, i, row) => (
+            <span className="nav-link" key={link.name}>
+              <a className={currPage === link.name ? "link nav-link nav-current" : "link nav-link"} href={link.url}>
+                {link.name}
+              </a>
+              {i + 1 !== row.length && !mobileNavVisible && <span>&nbsp;/&nbsp;</span>}
+            </span>
+          ))}
+          {mobileNavVisible ? <hr /> : <></>}
+          {mobileNavVisible ? (
+            moreLinks.map((link, i, row) => (
               <span className="nav-link" key={link.name}>
-                <a className={(currPage === link.name) ? "link nav-link nav-current" : "link nav-link"} href={link.url}>{link.name}</a>
-                {(i + 1 !== row.length) && (!mobileNavVisible) && (<span>&nbsp;/&nbsp;</span>)}
+                <a className={currPage === link.name ? "link nav-link nav-current" : "link nav-link"} href={link.url}>
+                  {link.name}
+                </a>
+                {i + 1 !== row.length && !mobileNavVisible && <span>&nbsp;/&nbsp;</span>}
               </span>
             ))
-          }
-          {(mobileNavVisible) ? <hr /> : <></>}
-          {
-            (mobileNavVisible) ? moreLinks.map((link, i, row) => (
-              <span className="nav-link" key={link.name}>
-                <a className={(currPage === link.name) ? "link nav-link nav-current" : "link nav-link"} href={link.url}>{link.name}</a>
-                {(i + 1 !== row.length) && (!mobileNavVisible) && (<span>&nbsp;/&nbsp;</span>)}
-              </span>
-            )) : <></>
-          }
+          ) : (
+            <></>
+          )}
         </div>
         <a className="link nav-link nav-more" href="#" onClick={() => setMoreMenuVisible(!moreMenuVisible)}>
-          more
+          {moreMenuVisible ? "less" : "more"}
         </a>
       </div>
-      <ul className={`nav-more-menu ${(moreMenuVisible) ? 'nav-visible' : ''}`}>
-        {
-          moreLinks.map((link, i, row) => (
-            <li className="nav-link" key={link.name}>
-              <a className={(currPage === link.name) ? "link nav-link nav-current" : "link nav-link"} href={link.url}>{link.name}</a>
-            </li>
-          ))
-        }
+      <ul className={`nav-more-menu ${moreMenuVisible ? "nav-visible" : ""}`}>
+        {moreLinks.map((link, i, row) => (
+          <li className="nav-link" key={link.name}>
+            <a className={currPage === link.name ? "link nav-link nav-current" : "link nav-link"} href={link.url}>
+              {link.name}
+            </a>
+          </li>
+        ))}
       </ul>
       <div className="nav-container-mobile">
-        <button className={`hamburger hamburger--vortex ${(mobileNavVisible) ? 'is-active' : ''}`} type="button"
-          aria-label="Menu" aria-controls="navigation" aria-expanded={mobileNavVisible} onClick={mobileClick}>
+        <button
+          className={`hamburger hamburger--vortex ${mobileNavVisible ? "is-active" : ""}`}
+          type="button"
+          aria-label="Menu"
+          aria-controls="navigation"
+          aria-expanded={mobileNavVisible}
+          onClick={mobileClick}
+        >
           <span className="hamburger-box">
             <span className="hamburger-inner"></span>
           </span>
         </button>
       </div>
-    </header >
-  );
+    </header>
+  )
 }
 
 export default Header
--- src/components/visitors.tsx
+++ src/components/visitors.tsx
@@ -1,40 +1,37 @@
-import { useEffect, useState } from "react";
-import Button from "./button";
-import "../sass/visitors.scss";
+import { useEffect, useState } from "react"
+import Button from "./button"
+import "../sass/visitors.scss"
 
 const Visitors = () => {
-    const [visitors, setVisitors] = useState("");
-    const [btnVisible, setBtnVisible] = useState(true);
+  const [visitors, setVisitors] = useState("")
+  const [btnVisible, setBtnVisible] = useState(true)
 
-    async function getVisitors() {
-        const res = await fetch('https://api.bencuan.me/get-claps');
-        setVisitors(await res.text());
-        console.log(visitors);
-    }
-    useEffect(() => {
-        getVisitors();
+  async function getVisitors() {
+    const res = await fetch("https://api.bencuan.me/get-claps")
+    setVisitors(await res.text())
+    console.log(visitors)
+  }
+  useEffect(() => {
+    getVisitors()
+  })
+
+  const updateClaps = () => {
+    fetch("https://api.bencuan.me/update-claps", {
+      method: "POST",
     })
+    getVisitors()
+    setBtnVisible(false)
+  }
 
-    const updateClaps = () => {
-        fetch('https://api.bencuan.me/update-claps', {
-            method: "POST",
-        });
-        getVisitors();
-        setBtnVisible(false);
-    }
+  const display = btnVisible ? (
+    <>
+      {`> ${visitors} people were here before you`} <Button onClick={updateClaps}>🙋 hey i'm here too!</Button>
+    </>
+  ) : (
+    <span className="including-you">{`> ${visitors} people were here including you :)`}</span>
+  )
 
-
-    const display = ((btnVisible) ? (
-        <>
-            {`> ${visitors} people were here before you`} <Button onClick={updateClaps} >🙋 hey i'm here too!</Button>
-        </>
-    ) : (
-        <span className="including-you">
-            {`> ${visitors} people were here including you :)`}
-        </span>
-    ));
-    
-    return (visitors) ? display : <></>
+  return visitors ? display : <></>
 }
 
 export default Visitors
