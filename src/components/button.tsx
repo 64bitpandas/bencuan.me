03fbac1a44ff398a80ed3254134d15530d271273
Ben Cuan <github@bencuan.me>
2024-02-17 20:22:48 +0000

    fun buttons and good prettier

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,19 +1,28 @@
 import type { MouseEventHandler, ReactNode } from "react"
-import "../sass/button.scss";
+import "../sass/button.scss"
 
 type props = {
-    href?: string,
-    onClick?: MouseEventHandler,
-    className?: string,
-    children?: ReactNode,
+  href?: string
+  onClick?: MouseEventHandler
+  className?: string
+  children?: ReactNode
 }
-const Button = ({href, onClick, className, children}: props) => {
-
-    if (href) {
-        return <a href={href} className={`button ${className}`} target="_blank" rel="noopener">{children}</a>
-    } else {
-        return (<span className={`button ${className}`} onClick={onClick}>{children}</span>)
-    }
+const Button = ({ href, onClick, className = "", children }: props) => {
+  if (href) {
+    return (
+      <button className={`button ${className}`}>
+        <a href={href} target="_blank" rel="noopener">
+          {children}
+        </a>
+      </button>
+    )
+  } else {
+    return (
+      <button className={`button ${className}`} onClick={onClick}>
+        {children}
+      </button>
+    )
+  }
 }
 
 export default Button
