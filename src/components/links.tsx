6690f8fc62e0e869e2b28bd5be9fc26241a60203
Ben Cuan <github@bencuan.me>
2024-02-18 03:06:56 +0000

    mobile debugging meta tag

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -1,47 +1,44 @@
 import { type ReactNode } from "react"
-import { Tooltip } from 'react-tooltip'
-import {v4 as uuid } from 'uuid';
-import '../sass/links.scss';
+import { v4 as uuid } from "uuid"
+import "../sass/links.scss"
 
 type xProps = {
-  href: string,
-  label: string,
-  children?: ReactNode,
-  className?: string,
-  hasArrow?: boolean,
+  href: string
+  label: string
+  children?: ReactNode
+  className?: string
+  hasArrow?: boolean
 }
 
 /** Link to an external website. */
-export const XLink = ({ href, label, children, className, hasArrow=true}: xProps) => {
-  const id = uuid().toString().substring(0, 6);
+export const XLink = ({ href, label, children, className, hasArrow = true }: xProps) => {
+  const id = uuid().toString().substring(0, 6)
   return (
     <>
       <a
-        className={(className) ? className: "link"}
+        className={className ? className : "link"}
         href={href}
         target="_blank"
         rel="noreferrer"
         aria-label={label}
         id={`link-${id}`}
       >
-        {children}{hasArrow && (<>&nbsp;↗</>)}
+        {children}
+        {hasArrow && <>&nbsp;↗</>}
       </a>
-      <Tooltip anchorSelect={`#link-${id}`} className="custom-tooltip" clickable>
-        <a href={href} target="_blank" rel="noreferrer" aria-label={label}>{href}</a>
-      </Tooltip>
     </>
   )
 }
 
 type iProps = {
-  href: string,
-  children?: ReactNode,
-  className?: string,
+  href: string
+  children?: ReactNode
+  className?: string
 }
 
 /** Link to an internal page. */
 export const ILink = ({ href, children, className }: iProps) => (
-  <a href={href} className={(className) ? className : "internal-link"}>
+  <a href={href} className={className ? className : "internal-link"}>
     {children}
   </a>
 )
