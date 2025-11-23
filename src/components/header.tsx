98e22029afa260cfbea0c7d92b66f86bb46e18ac
Ben Cuan <github@bencuan.me>
2025-11-22 19:30:18 -0800

    fix 2021, make garden link open in new tab

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -29,18 +29,30 @@ type props = {
 const Header = ({ currPage, customPages }: props) => {
   const allLinks = customPages ? links.concat(customPages) : links;
 
+  const isExternalLink = (url: string) => {
+    return url.startsWith('http://') || url.startsWith('https://');
+  };
+
   return (
     <header>
       <div className="nav-container">
         <div className="nav-links">
-          {allLinks.map((link, i, row) => (
-            <span className="nav-link" key={link.name}>
-              <a className={currPage === link.name ? 'link nav-link nav-current' : 'link nav-link'} href={link.url}>
-                {link.name}
-              </a>
-              {i + 1 !== row.length && <span> / </span>}
-            </span>
-          ))}
+          {allLinks.map((link, i, row) => {
+            const isExternal = isExternalLink(link.url);
+            return (
+              <span className="nav-link" key={link.name}>
+                <a
+                  className={currPage === link.name ? 'link nav-link nav-current' : 'link nav-link'}
+                  href={link.url}
+                  target={isExternal ? '_blank' : undefined}
+                  rel={isExternal ? 'noopener noreferrer' : undefined}
+                >
+                  {link.name}
+                </a>
+                {i + 1 !== row.length && <span> / </span>}
+              </span>
+            );
+          })}
         </div>
       </div>
     </header>
