69ae47da90a6f9bd6acc4c1dc505bb14fedfb6e2
Ben Cuan <github@bencuan.me>
2026-02-03 00:24:03 -0800

    ready for release

---
+++ src/components/SubstackEmbed.tsx
@@ -0,0 +1,19 @@
+export default function SubstackEmbed() {
+  return (
+    <iframe
+      src="https://bencuan.substack.com/embed"
+      width="480"
+      height="150"
+      style={{
+        border: '1px solid #EEE',
+        background: 'white',
+        display: 'block',
+        margin: '0 auto',
+        maxWidth: '100%',
+      }}
+      frameBorder="0"
+      scrolling="no"
+    />
+  );
+}
+
